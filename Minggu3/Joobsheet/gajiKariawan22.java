07fcb309df30e4da06a5b2293a6018e30827c143
Raditfiransah <raditfiransah180@gmail.com>
2024-09-15 00:53:35 +0700

    123

--- Minggu3/Joobsheet/gajiKariawan22.java
+++ Minggu3/Joobsheet/gajiKariawan22.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class gajiKariawan22 {
     public static void main(String[] args) {
         int jamKerja, upahPerJam, gajikariawan;
-        double bonus = 0.1, pajak = 0.05;
+        double persenBonus = 0.1, persenPajak = 0.05;
 
         Scanner input = new Scanner(System.in);
         jamKerja = input.nextInt();
         upahPerJam =input.nextInt();
 
         double totalGaji = jamKerja * upahPerJam;
-        
+        double bonus = (totalGaji+persenBonus);
+        double pajak = persenPajak * (totalGaji+bonus);
+        double gajiBersih = (totalGaji+bonus)-pajak;
+
+        System.out.println(totalGaji);
+        System.out.println(bonus);
+        System.out.println(pajak);
+        System.out.println(gajiBersih);
     }
 }
